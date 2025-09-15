1f0be4e97e72df006ab48dc0ceadc55ddadd0322
Maicon Souza <maiconsouzapa@gmail.com>
2025-09-15 14:43:44 -0300

    added comments

--- install_all.sh
+++ install_all.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Git config
 sudo ./generate_github_ssh_key.sh
 
+# Java config
 sudo ./install_java17.sh
 
+# Maven and Zsh sinstall
 sudo apt install -y maven zsh
 
+# Oh My Zsh install
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
